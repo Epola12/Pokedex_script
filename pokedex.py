2b05991efd1ac94e9c0977c16b166607578c614f
Eduardo Pola <joseeduardopola@hotmail.com>
2023-04-19 00:29:31 -0400

    update request function, needs a little rework is abble of getting the name so far of any given pokemon

--- pokedex.py
+++ pokedex.py
@@ -1,6 +1,12 @@
 import argparse
-import urllib3
-http=urllib3.PoolManager()
+import requests
+import json
+def poke_request_list(PH1):
+    r=requests.get('https://pokeapi.co/api/v2/pokemon-species/{N}'.format(N=PH1))
+    response=r.json()
+    test=response.get('name')
+    print(test)
+    return test
 parser = argparse.ArgumentParser()
 parser.add_argument('list',type=str,
                     help='Display the list of all the Pokemon')
@@ -9,17 +15,14 @@ parser.add_argument('-n','--number',type=int,
 args=parser.parse_args()
 given_pokemon=args.number
 if args.number:
-    print('given pokemon')
+    print('You selected')
     print(args.number)
+    print(poke_request_list(given_pokemon))
 else:
     print('all pokemon')
 
 #function to execute the API request depending of what if being pass
 #TODO finish this function
-def poke_request():
 
-    pass
-#no se si vas a ver el mensaje del commit pero si no lo pongo aqui gracias por ser
-#ese maravilloso amigo y mentor de esto que es mi sueño lograrlo, eres mi idolo hermano
-#sin duda un ejemplo a seguir gracias por tu apoyo ya no dejare este pedo
-#XOXO
+    
+
