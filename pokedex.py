4028dff0830e4dd33bc33ab330de034659ea731e
Eduardo Pola <joseeduardopola@hotmail.com>
2023-06-27 22:23:13 -0600

    Added the new parser and sub parser setting function of request working

--- pokedex.py
+++ pokedex.py
@@ -1,28 +1,53 @@
 import argparse
 import requests
 import json
+
+#TODO: investigate list comprehension 
 def poke_request_list(PH1):
-    r=requests.get('https://pokeapi.co/api/v2/pokemon-species/{N}'.format(N=PH1))
-    response=r.json()
-    test=response.get('name')
-    print(test)
-    return test
+    if PH1==True: 
+        r=requests.get('https://pokeapi.co/api/v2/pokemon-species/{N}'.format(N=given_pokemon))
+        response=r.json()
+        poke_name=response.get('name')
+        return poke_name
+    elif PH1==False:
+        r2=requests.get('https://pokeapi.co/api/v2/pokemon-species',params={'limit':151})
+        response2=r2.json()
+        poke_list=response2.get('results')
+        list=[]
+        for i in poke_list:
+            list.append(i['name'])
+        return list
+
+def poke_list_ID():
+    pass
+#Parsers part of the code where the parser are set 
+#main parser set 
 parser = argparse.ArgumentParser()
-parser.add_argument('list',type=str,
-                    help='Display the list of all the Pokemon')
-parser.add_argument('-n','--number',type=int,
-                    help='Display the information of the given pokemon')
+parser.add_argument('pokedex',type=str,
+                    help='Command to access pokedex information')
+#setting subparsers
+subparser=parser.add_subparsers(help='sub-command help')
+sub_list=subparser.add_parser('list',help='Display a list of all the 1rs gen Pokemon')
+sub_list.add_argument('-n','--number',type=int, help='Display the given pokemon')
 args=parser.parse_args()
 given_pokemon=args.number
-if args.number:
-    print('You selected')
-    print(args.number)
-    print(poke_request_list(given_pokemon))
-else:
-    print('all pokemon')
 
-#function to execute the API request depending of what if being pass
-#TODO finish this function
-
+def main():
+    if args.number and given_pokemon>151:
+        print('You can only select pokemon from 1st generation')
+        print('Try again')
+    elif args.number:
+        print('------You selected------')
+        print('Pokemon Name: {Name} --- Pokemon ID: {ID}'.format(Name=poke_request_list(True),ID=args.number))
+    elif args.number==None:
+        pokemon_list=poke_request_list(False)
+        print('------all 1st Gen pokemon------')
+        print('ID---------NAME')
+        for i in pokemon_list:
+            print(i)
+           
+            
     
+if __name__== '__main__':
+    main()    
 
