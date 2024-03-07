63042da2b54f943f96bb0046e2af6ec30acfe062
Eduardo Pola <joseeduardopola@hotmail.com>
2024-03-07 13:57:36 -0600

    last updated file from work computer, previous commit was the most stable version

--- pokedex.py
+++ pokedex.py
@@ -3,9 +3,11 @@ import requests
 import json
 
 #TODO: investigate list comprehension 
+
+#setting of the requests funtion according to the data nedeed 
 def poke_request_list(PH1):
     if PH1==True: 
-        r=requests.get('https://pokeapi.co/api/v2/pokemon-species/{N}'.format(N=given_pokemon))
+        r=requests.get('https://pokeapi.co/api/v2/pokemon-species/{N}'.format(N=args.number))
         response=r.json()
         poke_name=response.get('name')
         return poke_name
@@ -17,29 +19,17 @@ def poke_request_list(PH1):
         for i in poke_list:
             list.append(i['name'])
         return list
-
+    
 def poke_list_ID(pokemon_name):
     id_request=requests.get('https://pokeapi.co/api/v2/pokemon/{poke_name}'.format(poke_name=pokemon_name))
     id_response=id_request.json()
     id=id_response.get('id')
     return id
-    
-#Parsers part of the code where the parser are set 
-#main parser set 
-parser = argparse.ArgumentParser()
-parser.add_argument('pokedex',type=str,
-                    help='Command to access pokedex information')
-#setting subparsers
-subparser=parser.add_subparsers(help='sub-command help')
-sub_list=subparser.add_parser('list',help='Display a list of all the 1rs gen Pokemon')
-sub_list.add_argument('-n','--number',type=int, help='Display the given pokemon')
-sub_evo=subparser.add_parser('evolution',help='Show the given pokemon evolution in a table')
-sub_evo.add_argument('-t','--table', help='')
-args=parser.parse_args()
-given_pokemon=args.number
 
-def main():
-    if args.number and given_pokemon>151:
+#setting the main function to run according of the commmand 
+def poke_list():
+    print('aqui')
+    if args.number>151:
         print('You can only select pokemon from 1st generation')
         print('Try again')
     elif args.number:
@@ -52,8 +42,31 @@ def main():
         for i in pokemon_list:
             pokemon_id=poke_list_ID(i)
             id_str=str(pokemon_id)
-            print(id_str.zfill(3))
+            print(id_str.zfill(3),i)
+
+
+def poke_evolution():
+    if args.table ==None:
+        print('You are missing the pokemon ID and the argument -t or --table')
+
+    
+#Parsers - part of the code where the parser are set 
+#main parser set 
+parser = argparse.ArgumentParser(prog='Pokedex CLI', description='CLI por getting pokemon data', epilog='Thanks see  you next time')
+parser.add_argument('pokedex',type=str,
+                    help='Command to access pokedex information')
+#setting subparsers
+subparser=parser.add_subparsers(help='sub-command help')
+sub_list=subparser.add_parser('list',help='Display a list of all the 1rs gen Pokemon')
+sub_list.add_argument('-n','--number',type=int, help='Display the given pokemon')
+sub_list.set_defaults(func=poke_list)
+sub_evo=subparser.add_parser('evolution',help='Show the given pokemon evolution in a table')
+sub_evo.add_argument('-t','--table', help='Show the given pokemon evolution in a table')
+args=parser.parse_args()
+
+#def main(): 
         
-if __name__== '__main__':
-    main()    
+              
+#if __name__== '__main__':
+ #   main()    
 
